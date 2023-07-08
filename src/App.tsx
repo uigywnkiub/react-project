20598186456c9c8e11d39d39b16afd93c94c2222
Volodymyr Gerun <vgerun97@gmail.com>
2023-07-08 14:46:33 +0300

    add a hint if form data is invalid

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from "react";
 import {
   Routes,
   Route,
@@ -7,26 +7,26 @@ import {
   useLocation,
   Navigate,
   Outlet,
-} from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
-import { ThemeProvider } from '@mui/material/styles'
-import Input from '@mui/material/Input'
-import Button from '@mui/material/Button'
-import { BallTriangle } from 'react-loader-spinner'
-import { RxAvatar } from 'react-icons/rx'
+} from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { ThemeProvider } from "@mui/material/styles";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+import { BallTriangle } from "react-loader-spinner";
+import { RxAvatar } from "react-icons/rx";
 
-import NotFound from './components/NotFound'
-import Quotes from './components/Quotes'
-import SearchEngine from './components/SearchEngine'
-import LanguageSwitcher from './components/LanguageSwitcher'
-import GameFight from './components/GameFight'
+import NotFound from "./components/NotFound";
+import Quotes from "./components/Quotes";
+import SearchEngine from "./components/SearchEngine";
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import GameFight from "./components/GameFight";
 
-import { useAppSelector } from './hook'
-import { fakeAuthProvider } from './API/auth'
-import { theme } from './assets/theme'
+import { useAppSelector } from "./hook";
+import { fakeAuthProvider } from "./API/auth";
+import { theme } from "./assets/theme";
 
 export default function App() {
-  const storeIsModalOpen = useAppSelector((state) => state.store.isModalOpen)
+  const storeIsModalOpen = useAppSelector((state) => state.store.isModalOpen);
 
   return (
     <div className="auth-container">
@@ -34,7 +34,7 @@ export default function App() {
         <a
           className="github-link"
           href="https://github.com/kas1qqqq/react-project"
-          target={'_blank'}
+          target={"_blank"}
         >
           GitHub
         </a>
@@ -42,7 +42,7 @@ export default function App() {
 
       {storeIsModalOpen && <LanguageSwitcher />}
 
-      <h1 style={{ color: '#A5A0FF' }}>React Project</h1>
+      <h1 style={{ color: "#A5A0FF" }}>React Project</h1>
 
       <AuthProvider>
         <Routes>
@@ -64,12 +64,12 @@ export default function App() {
         </Routes>
       </AuthProvider>
     </div>
-  )
+  );
 }
 
 function Layout() {
-  const { t } = useTranslation()
-  const location = useLocation()
+  const { t } = useTranslation();
+  const location = useLocation();
 
   return (
     <div className="auth-container">
@@ -77,113 +77,113 @@ function Layout() {
 
       <ul className="link-container">
         <li>
-          <Link className={location.pathname === '/' ? 'active' : ''} to="/">
-            {t('homePage.titleLink')}
+          <Link className={location.pathname === "/" ? "active" : ""} to="/">
+            {t("homePage.titleLink")}
           </Link>
         </li>
         <li>
           <Link
-            className={location.pathname === '/news' ? 'active' : ''}
+            className={location.pathname === "/news" ? "active" : ""}
             to="/news"
           >
-            {t('newsPage.titleLink')}
+            {t("newsPage.titleLink")}
           </Link>
         </li>
         <li>
           <Link
-            className={location.pathname === '/games/fight' ? 'active' : ''}
+            className={location.pathname === "/games/fight" ? "active" : ""}
             to="/games/fight"
           >
-            {t('gamesPage.titleLink')}
+            {t("gamesPage.titleLink")}
           </Link>
         </li>
         <li>
           <Link
-            className={location.pathname === '/profile' ? 'active' : ''}
+            className={location.pathname === "/profile" ? "active" : ""}
             to="/profile"
           >
-            {t('profilePage.titleLink')}
+            {t("profilePage.titleLink")}
           </Link>
         </li>
       </ul>
 
       <Outlet />
     </div>
-  )
+  );
 }
 
 interface AuthContextType {
-  user: string | null
-  userPass: string | null
-  signin: (user: string, password: string, callback: VoidFunction) => void
-  signout: (callback: VoidFunction) => void
+  user: string | null;
+  userPass: string | null;
+  signin: (user: string, password: string, callback: VoidFunction) => void;
+  signout: (callback: VoidFunction) => void;
 }
 
-const AuthContext = React.createContext<AuthContextType>(null!)
+const AuthContext = React.createContext<AuthContextType>(null!);
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
-  let [user, setUser] = React.useState<string | null>(null)
-  let [userPass, setUserPass] = React.useState<string | null>(null)
+  let [user, setUser] = React.useState<string | null>(null);
+  let [userPass, setUserPass] = React.useState<string | null>(null);
 
   const signin = (user: string, userPass: string, callback: VoidFunction) => {
     return fakeAuthProvider.signin(() => {
-      setUser(user)
-      setUserPass(userPass)
-      callback()
-    })
-  }
+      setUser(user);
+      setUserPass(userPass);
+      callback();
+    });
+  };
 
   const signout = (callback: VoidFunction) => {
     return fakeAuthProvider.signout(() => {
-      setUser(null)
-      setUserPass(null)
-      callback()
-    })
-  }
+      setUser(null);
+      setUserPass(null);
+      callback();
+    });
+  };
 
-  const value = { user, userPass, signin, signout }
+  const value = { user, userPass, signin, signout };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 function useAuth() {
-  return React.useContext(AuthContext)
+  return React.useContext(AuthContext);
 }
 
 function AuthStatus() {
   const {
     t,
     i18n: { language: ln },
-  } = useTranslation()
+  } = useTranslation();
 
-  const auth = useAuth()
-  const navigate = useNavigate()
-  const [isAuth, setIsAuth] = React.useState<boolean>(false)
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const [isAuth, setIsAuth] = React.useState<boolean>(false);
 
-  const userLocal = localStorage.getItem('username')
+  const userLocal = localStorage.getItem("username");
 
   const artificialDelay = () => {
-    setTimeout(() => (!isAuth ? setIsAuth(false) : setIsAuth(true)), 0)
-  }
+    setTimeout(() => (!isAuth ? setIsAuth(false) : setIsAuth(true)), 0);
+  };
 
   if (!auth.user && !userLocal) {
-    return <p>{t('notLogIn')}</p>
+    return <p>{t("notLogIn")}</p>;
   }
 
   const navigateHandler = () => {
-    setIsAuth(true)
+    setIsAuth(true);
 
     auth.signout(() => {
-      artificialDelay()
-      navigate('/')
-    })
-  }
+      artificialDelay();
+      navigate("/");
+    });
+  };
 
   return (
     <div className="auth-success-container">
       <div className="auth-success-title">
         <p>
-          {t('welcomeUser')} {!userLocal ? auth.user : userLocal}
+          {t("welcomeUser")} {!userLocal ? auth.user : userLocal}
         </p>
         <RxAvatar />
       </div>
@@ -196,11 +196,11 @@ function AuthStatus() {
           onClick={navigateHandler}
         >
           {!isAuth ? (
-            <span className="my-lowercase">{t('signOut')}</span>
+            <span className="my-lowercase">{t("signOut")}</span>
           ) : (
             <BallTriangle
               height={28}
-              width={ln === 'en' ? 63 : 47.5}
+              width={ln === "en" ? 63 : 47.5}
               radius={5}
               color="#646cff"
               ariaLabel="ball-triangle-loading"
@@ -210,115 +210,117 @@ function AuthStatus() {
         </Button>
       </ThemeProvider>
     </div>
-  )
+  );
 }
 
 function RequireAuth({ children }: { children: JSX.Element }) {
-  const location = useLocation()
-  const auth = useAuth()
+  const location = useLocation();
+  const auth = useAuth();
 
-  let isAuth = fakeAuthProvider.isAuthenticated
+  let isAuth = fakeAuthProvider.isAuthenticated;
 
-  const userLocal = localStorage.getItem('username')?.toString()
-  const userPassLocal = localStorage.getItem('password')?.toString()
+  const userLocal = localStorage.getItem("username")?.toString();
+  const userPassLocal = localStorage.getItem("password")?.toString();
 
-  if (userLocal && userPassLocal) isAuth = true
+  if (userLocal && userPassLocal) isAuth = true;
 
   if (!auth.user && !auth.userPass && !isAuth) {
-    return <Navigate to="/login" state={{ from: location }} replace />
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  return children
+  return children;
 }
 
 function LoginPage() {
-  const [loginLenght, setLoginLenght] = React.useState<boolean>(false)
-  const [isLoginValid, setIsLoginValid] = React.useState<boolean>(false)
+  const [loginLength, setLoginLength] = React.useState<boolean>(false);
+  const [isLoginValid, setIsLoginValid] = React.useState<boolean>(false);
 
-  const [passLenght, setPassLenght] = React.useState<boolean>(false)
-  const [isPassValid, setIsPassValid] = React.useState<boolean>(false)
+  const [passLength, setPassLength] = React.useState<boolean>(false);
+  const [isPassValid, setIsPassValid] = React.useState<boolean>(false);
 
-  const [isFormValid, setIsFormValid] = React.useState<boolean>(false)
+  const [isFormValid, setIsFormValid] = React.useState<boolean>(false);
 
-  const navigate = useNavigate()
-  const location = useLocation()
-  const auth = useAuth()
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
 
   const {
     t,
     i18n: { language: ln },
-  } = useTranslation()
+  } = useTranslation();
 
-  let from = location.state?.from?.pathname || '/'
+  let from = location.state?.from?.pathname || "/";
 
   // if need to add many classes, use the case below
   // const className = ['my-input', isLoginValid ? 'isFormValid' : ''].join(' ')
 
   // don't use arrow func, cause it haven't build-in < ...args > param
   function classNames(...args: any[]) {
-    return args.filter(Boolean).join(' ')
+    return args.filter(Boolean).join(" ");
   }
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const formData = new FormData(event.currentTarget)
-    const username = formData.get('username') as string
-    const password = formData.get('password') as string
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
 
     if (username.length !== 5) {
-      setLoginLenght(true)
+      setLoginLength(true);
     } else {
-      setLoginLenght(false)
+      setLoginLength(false);
     }
 
     if (username !== import.meta.env.VITE_USERNAME_KEY) {
-      setIsLoginValid(true)
+      setIsLoginValid(true);
     } else {
-      setIsLoginValid(false)
+      setIsLoginValid(false);
     }
 
     if (password.length !== 5) {
-      setPassLenght(true)
+      setPassLength(true);
     } else {
-      setPassLenght(false)
+      setPassLength(false);
     }
 
     if (password !== import.meta.env.VITE_PASSWORD_KEY) {
-      setIsPassValid(true)
+      setIsPassValid(true);
     } else {
-      setIsLoginValid(false)
+      setIsLoginValid(false);
     }
 
     if (
       username === import.meta.env.VITE_USERNAME_KEY &&
       password === import.meta.env.VITE_PASSWORD_KEY
     ) {
-      setIsPassValid(false)
-      setIsFormValid(true)
+      setIsPassValid(false);
+      setIsFormValid(true);
 
-      localStorage.setItem('username', username)
-      localStorage.setItem('password', password)
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
 
       auth.signin(username, password, () => {
         // use { replace: true } to avoid redirecting to login if you're already in
-        navigate(from, { replace: true })
-      })
+        navigate(from, { replace: true });
+      });
     }
-  }
+  };
 
   return (
     <div>
       <p className="page-description">
-        {t('profilePage.logInToViewProfilePage')} {from}
+        {t("profilePage.logInToViewProfilePage")} {from}
       </p>
+
+      {isPassValid && <span className="hint-account-data">{t("hintAccountData")}</span>}
 
       <form className="form-container" onSubmit={submitHandler}>
         <ThemeProvider theme={theme}>
           <Input
-            className={classNames('my-input', isFormValid && 'form-valid')}
+            className={classNames("my-input", isFormValid && "form-valid")}
             placeholder={
-              ln === 'en' ? t('username').toString() : t('username').toString()
+              ln === "en" ? t("username").toString() : t("username").toString()
             }
             name="username"
             color="primary"
@@ -330,15 +332,15 @@ function LoginPage() {
         </ThemeProvider>
 
         <div className="form-error">
-          {loginLenght && <span>{t('formError.loginLength')}</span>}
-          {isLoginValid && <span>{t('formError.loginInvalid')}</span>}
+          {loginLength && <span>{t("formError.loginLength")}</span>}
+          {isLoginValid && <span>{t("formError.loginInvalid")}</span>}
         </div>
 
         <ThemeProvider theme={theme}>
           <Input
-            className={classNames('my-input', isFormValid && 'form-valid')}
+            className={classNames("my-input", isFormValid && "form-valid")}
             placeholder={
-              ln === 'en' ? t('password').toString() : t('password').toString()
+              ln === "en" ? t("password").toString() : t("password").toString()
             }
             name="password"
             color="primary"
@@ -350,8 +352,8 @@ function LoginPage() {
         </ThemeProvider>
 
         <div className="form-error">
-          {passLenght && <span>{t('formError.passLength')}</span>}
-          {isPassValid && <span>{t('formError.passInvalid')}</span>}
+          {passLength && <span>{t("formError.passLength")}</span>}
+          {isPassValid && <span>{t("formError.passInvalid")}</span>}
         </div>
 
         <ThemeProvider theme={theme}>
@@ -362,7 +364,7 @@ function LoginPage() {
             variant="contained"
           >
             {!isFormValid ? (
-              <span className="my-lowercase">{t('loginBtn')}</span>
+              <span className="my-lowercase">{t("loginBtn")}</span>
             ) : (
               <BallTriangle
                 height={28}
@@ -377,40 +379,40 @@ function LoginPage() {
         </ThemeProvider>
       </form>
     </div>
-  )
+  );
 }
 
 // use stock func cause it means a component
 function HomePage() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <>
-      <h3>{t('homePage.title')}</h3>
+      <h3>{t("homePage.title")}</h3>
       <SearchEngine />
     </>
-  )
+  );
 }
 
 function GamesPage() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <>
-      <h3>{t('gamesPage.title')}</h3>
-      <p className="page-description">{t('gamesPage.fightDescription')}</p>
+      <h3>{t("gamesPage.title")}</h3>
+      <p className="page-description">{t("gamesPage.fightDescription")}</p>
       <GameFight />
     </>
-  )
+  );
 }
 
 function ProfilePage() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <>
-      <h3>{t('profilePage.title')}</h3>
-      <p className="page-description">{t('profilePage.description')}</p>
+      <h3>{t("profilePage.title")}</h3>
+      <p className="page-description">{t("profilePage.description")}</p>
     </>
-  )
+  );
 }
